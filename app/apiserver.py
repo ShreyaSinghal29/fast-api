bbbabf699c427d9201f431225de913123f9e6837
ShreyaSinghal29 <shreyaasinghal29@gmail.com>
2025-04-05 10:02:01 +0530

    Fix: Wait until FastAPI server is live before testing

--- app/apiserver.py
+++ app/apiserver.py
@@ -16,4 +16,4 @@ def multiply(a: int, b: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("app.apiserver:app", host="127.0.0.1", port=8000,reload=False)
